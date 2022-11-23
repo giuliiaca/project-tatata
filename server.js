f39ce2280bed4d0cbc66cf1466b5f528265e5f23
giuliiaca <giuliana.concialini@mail.polimi.it>
2022-11-23 13:32:23 +0100

    added server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let express = require("express");
 
 let app = express(); //we are activating express, and put it in a variable
 
-let port = 3000; //choose the port
+let port = process.env.PORT || 3000; //choose the port
 
 let server = app.listen(port); //posso scrivere anche(3000) port is the input
 
